96601473fd29f823bb286e535685ddf46d3abe5b
Aizhan Tilek <aijanm01@gmail.com>
2021-07-05 13:35:32 +0600

    Refactor Contact page

--- pricer/src/localization/lang/en.js
+++ pricer/src/localization/lang/en.js
@@ -83,5 +83,55 @@ export default {
       }
     ],
      btn: 'List of Services'
+  },
+  about: {
+    title: 'Company Information',
+    subTitle: 'Pull out your potential',
+    link: {
+      home: 'Home',
+      contact: 'Company Information'
+    },
+    info: {
+      companyName: {
+        key: 'Company Name',
+        value: 'PRICER BISHKEK LLC'
+      },
+      establishmentDate: {
+        key: 'Establishment Date',
+        value: '2019.08.09'
+      },
+      CEO: {
+        key: 'CEO',
+        value: 'Yuya KOMATSU'
+      },
+      numberofStaff: {
+        key: 'Number of Staff',
+        value: '21 (on 2021.02.04)'
+      },
+      businessDescription: {
+        key: 'Business Description',
+        value: 'Design / System development / App development / Server maintenance'
+      },
+      location: {
+        key: 'Location',
+        value: 'Kyrgyzstan, Bishkek city, 109/3, Turusbekova street, 106'
+      },
+      phoneNumber: {
+        key: 'Phone Number',
+        value: 'Tel: +996 (312) 988999'
+      },
+      socialNetworks: {
+        key: 'Social Networks',
+        value: 'INSTA'
+      },
+      participatingOrganizations: {
+        key: 'Participating Organizations',
+        value: 'High Technology Park of the Kyrgyz Republic KSSDA'
+      },
+      history: {
+        key: 'History',
+        value: '2019 August Start of the Pricer Bishkek'
+      }
+    }
   }
 }
